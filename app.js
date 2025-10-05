5d9a3c8ae4288ed8a18c692a6176da264485d90f
Nikita Pizarro <nikitta6928@gmail.com>
2025-10-05 16:29:12 -0300

    unid2

--- app.js
+++ app.js
@@ -16,21 +16,44 @@ const writeDb = data => fs.writeFileSync("./products.json", JSON.stringify(data)
 
 //Status
 server.get("/", (request, response) => {
-    response.json({ status: "false" })
+    response.json({ status: false })
 
 })
 
-//add product.  Método get
+// get product
 server.get("/products", (request, response) => {
-
     response.json(products)
+
 })
-//Método post
+
+
+//add product.  
 server.post("/products", (request, response) => {
     const body = request.body
-    console.log(body)
+
+    const newProduct = { id: crypto.randomUUID(), ...body }
+    products.push(newProduct)
+    writeDb(products)
 
     response.json({ data: "agregando productos!" })
+})
+
+//Método patch
+server.patch("/products/:id", (request, response) => {
+    const body = request.body
+    const id = request.params.id
+
+    const index = products.findIndex(product => product.id === id)
+
+    products[index] = { ...products[index], ...body }
+
+    console.log(products[index])
+
+    response.json({ status: "Actualizando un producto!" })
+
+
+
+
 })
 
 server.listen(1111, () => {
